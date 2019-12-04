bc22fedb0e21a1bbc89f3a3986dee78f54e1d212
wansc <858269620@qq.com>
2019-12-04 16:46:46 +0800

    生成Repository代码并打成zip包

--- src/main/java/cn/iba8/module/generator/controller/CodeGenerateController.java
+++ src/main/java/cn/iba8/module/generator/controller/CodeGenerateController.java
@@ -2,14 +2,20 @@ package cn.iba8.module.generator.controller;
 
 import cn.iba8.module.generator.common.ftl.TemplateDefinition;
 import cn.iba8.module.generator.common.response.BaseResponse;
-import cn.iba8.module.generator.common.util.ZipUtil;
+import cn.iba8.module.generator.common.util.FileNameUtil;
+import cn.iba8.module.generator.common.util.SpringUtils;
+import cn.iba8.module.generator.config.CodeGeneratorProperties;
 import cn.iba8.module.generator.service.CodeGenerateService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.List;
 
 @RestController
@@ -22,7 +28,25 @@ public class CodeGenerateController {
     @GetMapping("/codeGenerate/codes")
     public BaseResponse<Void> generate(String moduleCode, String version, String typeGroup, HttpServletResponse response) {
         List<TemplateDefinition.TemplateFileBean> templateFileBeans = codeGenerateService.getCodeFiles(moduleCode, version, typeGroup);
-        ZipUtil.filePackage(templateFileBeans, moduleCode, version, typeGroup);
+        CodeGeneratorProperties codeGeneratorProperties = SpringUtils.getBean(CodeGeneratorProperties.class);
+        String tmp = codeGeneratorProperties.getCodeOutputTmp() + "/" + System.currentTimeMillis();
+        if (!CollectionUtils.isEmpty(templateFileBeans)) {
+            for (TemplateDefinition.TemplateFileBean templateFileBean : templateFileBeans) {
+                try {
+                    String fileDir = templateFileBean.getFileDir();
+                    String path = tmp + "/" + FileNameUtil.toPath(fileDir);
+                    File parent = new File(path);
+                    if (!parent.exists()) {
+                        parent.mkdirs();
+                    }
+                    OutputStream os = new FileOutputStream(new File(path + "/" + templateFileBean.getFilename() + "." + typeGroup));
+                    os.write(templateFileBean.getContent().getBytes());
+                } catch (Exception e) {
+                    log.error("生成临时文件失败");
+                }
+            }
+        }
+
         return BaseResponse.success(null);
     }
 
